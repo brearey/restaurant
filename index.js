3d4d2015902aab1d00ee067d91eb4bdd01227d29
Brearey <brearey4@gmail.com>
2023-04-29 23:18:53 +0900

    User entity

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,9 +9,18 @@ const app = express();
 app.use(express.json());
 
 const bookingRouter = require('./routers/bookingRouter');
+const userRouter = require('./routers/userRouter');
 
 app.use('/booking', bookingRouter);
+app.use('/users', userRouter);
+
+// Test zone
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
+// Test zone end
 
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту: ${PORT}`)
-})
+});
