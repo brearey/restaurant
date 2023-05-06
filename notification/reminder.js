8af2dda1dcb45e2923ed8d9ad6520813471460ce
brearey <brearey4@gmail.com>
2023-05-06 09:50:09 +0900

    reminder compare date

--- notification/reminder.js
+++ notification/reminder.js
@@ -2,10 +2,19 @@
 import { scheduleJob } from 'node-schedule';
 
 export default function createReminder(booking_start) {
-    const date = getRemindTime(booking_start);
-    scheduleJob(date, function() {
-        console.log('ITS REMIND TIME!', Date.now());
-    });
+    try {
+        if (!compareBookingStartWithNow(booking_start)) {
+            return {
+                message: `Дата в прошедшем времени. Сейчас: ${Date()}`,
+            }
+        }
+        const date = getRemindTime(booking_start);
+        scheduleJob(date, function () {
+            console.log('ITS REMIND TIME!', Date.now());
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 function getRemindTime(booking_start) {
@@ -13,4 +22,11 @@ function getRemindTime(booking_start) {
     const millisPerHour = 3600000;
     // return new Date(date.getTime() - 2 * millisPerHour); // TODO uncomment to production
     return new Date(date.getTime() + 5000);
+}
+
+function compareBookingStartWithNow(booking_start) {
+    const date = new Date(booking_start);
+    const now = Date.now();
+
+    return date > now;
 }
---
+++ test.js
@@ -0,0 +1,3 @@
+import createReminder from "./notification/reminder.js";
+
+console.log( createReminder('2023-05-06T14:30:00.000+09:00') );
